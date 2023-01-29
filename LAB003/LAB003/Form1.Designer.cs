4f67e90dd43669e874d631921a3068e1add525f9
Natthawan.p <Natthawan.p@kkumail.com>
2023-01-29 23:47:43 +0700

    Natthawan

--- LAB003/LAB003/Form1.Designer.cs
+++ LAB003/LAB003/Form1.Designer.cs
@@ -122,7 +122,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(137, 88);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -133,7 +133,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(130, 88);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = " ";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
